b6afdfd369c34844b2a647e932c3d279d7e14d9f
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-16 00:00:45 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -25,8 +25,19 @@ function loadMapScenario() {
     map_manager.map = map;
     for(var i in map_manager.map_items) {
         var map_item = map_manager.map_items[i];
-        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/' + map_item["pokemon_id"] + '.png'});
+        var count_down = get_count_down_from_timestamp(map_item["expire"]);
+        var icon_url = 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/' + map_item["pokemon_id"] + '.png';
+        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
+                                                 {title : count_down, icon: icon_url});
         map.entities.push(pushpin);
     }
+}
+
+function get_count_down_from_timestamp(timestamp) {
+    var now_time = new Date().getTime() / 1000;
+    var time_left = timestamp - now_time;
+    var second = Math.floor(time_left % 60);
+    var minute = Math.floor(time_left / 60);
+    return minute + ':' + second;
 }
 
