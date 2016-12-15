3d57250c37ea76894f4ecc0709b1bf3b0540dafa
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-15 22:45:23 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,8 +2,7 @@ function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AjYOKYhwfvbEhwDlM3WpQP3zsgD9AgNUWIV40Fp0lw1WmwFjTIfc3sHP5GwhiW_Q'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/1.png',
-        anchor: new Microsoft.Maps.Point(12, 39) });
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/1.png'});
     map.entities.push(pushpin);
 }
 
