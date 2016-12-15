ea220af374b1eb6d4f574d881acc7f80dff35915
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-15 22:05:43 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,9 +1,3 @@
-// var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-//     credentials: 'AjYOKYhwfvbEhwDlM3WpQP3zsgD9AgNUWIV40Fp0lw1WmwFjTIfc3sHP5GwhiW_Q'
-// });
-// var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
-//     anchor: new Microsoft.Maps.Point(12, 39) });
-// map.entities.push(pushpin);
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
