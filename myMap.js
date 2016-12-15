ac9dd8bea63c51c64a9f503ce11295d8cec94757
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-15 22:07:28 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Your Bing Maps Key'
+        credentials: 'AjYOKYhwfvbEhwDlM3WpQP3zsgD9AgNUWIV40Fp0lw1WmwFjTIfc3sHP5GwhiW_Q'
     });
 }
