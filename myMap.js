7cfdf462c9a7ecb885e264bb0a2325ee1b3648d2
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-15 22:39:53 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,18 +1,9 @@
-// function loadMapScenario() {
-//     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-//         credentials: 'AjYOKYhwfvbEhwDlM3WpQP3zsgD9AgNUWIV40Fp0lw1WmwFjTIfc3sHP5GwhiW_Q'
-//     });
-//     var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
-//         anchor: new Microsoft.Maps.Point(12, 39) });
-//     map.entities.push(pushpin);
-// }
-
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AjYOKYhwfvbEhwDlM3WpQP3zsgD9AgNUWIV40Fp0lw1WmwFjTIfc3sHP5GwhiW_Q'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/1.png',
         anchor: new Microsoft.Maps.Point(12, 39) });
     map.entities.push(pushpin);
-
 }
+
