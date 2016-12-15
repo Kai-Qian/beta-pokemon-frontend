b5537a32e9774e77fa120dbfa860c89b8a1bede9
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-16 01:11:56 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -62,9 +62,11 @@ function query_pokemon_data() {
     apigClient.mapPokemonGet (params, body, additionalParams)
         .then(function(result){
             //This is where you would put a success callback
-        console.log(result);
+//         console.log(result);
+            map_manager.map_items = result;
         }).catch( function(result){
             //This is where you would put an error callback
+            console.log(result);
         });
 }
 
