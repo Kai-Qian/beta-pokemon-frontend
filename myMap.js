1752417641c72647af7780aa5ec28d5fc91532ce
Kai Qian <kaiq@andrew.cmu.edu>
2016-12-15 22:55:15 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,8 +1,32 @@
+var map_manager = {
+    "map":  null,
+    "map_items": []
+}
+
+map_manager.map_items = [
+    {
+        "pokemon_id" : 12,
+        "expire" : 1481813281,
+        "longitude" : 35.66,
+        "latitude" : 139.77
+    },
+    {
+        "pokemon_id" : 12,
+        "expire" : 1481813281,
+        "longitude" : 35.66,
+        "latitude" : 139.77
+    }
+
+]
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AjYOKYhwfvbEhwDlM3WpQP3zsgD9AgNUWIV40Fp0lw1WmwFjTIfc3sHP5GwhiW_Q'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/1.png'});
-    map.entities.push(pushpin);
+    map_manager.map = map;
+    for(var i in map_manager.map_items) {
+        var map_item = map_manager.map_items[i];
+        var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/' + map_item["pokemon_id"] + '.png'});
+        map.entities.push(pushpin);
+    }
 }
 
